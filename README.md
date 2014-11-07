b5e5ae76b7c6b28520f9c6cfb89e2ee74560ce7a
Alexander Krasnoyarov <sheo13666q@gmail.com>
2014-11-07 14:43:46 +0300

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 generate-robotstxt
 ==================
 
-Generator robots.txt for node js
+Generator robots.txt for node js and middleware for express/connect framework.
+
+## Installation
+
+    npm install generate-robotstxt
+
+## Usage middleware
+
+    var robotstxt = require('generate-robotstxt')
+
+    // Pass in the absolute path to your robots.txt file
+    app.use(robotstxt.middleware(__dirname + '/robots.txt'))
+
+## Usage generate
+
+    var robotstxt = require('generate-robotstxt')
+
+    // Pass in the absolute path to your robots.txt file
+    robotstxt({
+        policy: [
+            {
+                userAgent: '*',
+                allow: '/'
+            }
+        ],
+        sitemap: 'sitemap.xml',
+        crawlDelay: 100,
+        host: 'http://example.com',
+        cleanParam: 'ref /some_dir/get_book.pl',
+        dest: './'
+    }, callback)
+
---
+++ index.js
@@ -0,0 +1,117 @@
+'use strict';
+var fs = require('fs');
+var lodash = require('lodash');
+var crypto = require('crypto');
+
+function robotstxt(options, callback) {
+    options = lodash.defaults(options || {}, {
+        policy: [
+            {
+                userAgent: '*',
+                allow: '/'
+            }
+        ],
+        sitemap: null,
+        crawlDelay: null,
+        host: null,
+        cleanParam: null,
+        dest: './'
+    });
+
+    var addLine = function (name, rule) {
+        var contents = '';
+        if (Object.prototype.toString.call(name) === '[object Object]') {
+            Object.keys(name).forEach(function(key) {
+                var value = name[key];
+                contents += addLine(key, value);
+            });
+        } else if (rule && Object.prototype.toString.call(rule) === '[object Array]') {
+            rule.forEach(function (item) {
+                contents += addLine(name, item);
+            });
+        } else {
+            contents += capitaliseFirstLetter(name.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()) + ': ' + rule + '\n';
+        }
+        return contents;
+    };
+
+    var capitaliseFirstLetter = function (string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+
+    return (function() {
+        var contents = '';
+
+        options.policy.forEach(function (item) {
+            contents += addLine(item);
+        });
+
+        if (options.sitemap) {
+            contents += addLine('Sitemap', options.sitemap);
+        }
+
+        if (options.host) {
+            contents += addLine('Host', options.host);
+        }
+
+        if (options.crawDelay) {
+            if (typeof options.crawDelay === 'number' && isFinite(options.crawDelay)) {
+                contents += addLine('Crawl-delay', options.crawDelay);
+            } else {
+                throw new Error('gulp-robotstxt', 'Options Craw-delay must be integer or float');
+            }
+        }
+
+        if (options.cleanParam) {
+            contents += addLine('Clean-param', options.cleanParam);
+        }
+
+        fs.writeFile(options.dest + 'robots.txt', contents, function (error) {
+            if (error) {
+                throw error;
+            }
+
+            if (callback) {
+                callback(contents);
+            }
+        });
+    })();
+}
+
+function middleware(path, options) {
+    options = lodash.defaults(options || {}, {
+        path: 'robots.txt',
+        cache: true,
+        cacheMaxAge: 86400000
+    });
+
+    var robotstxt = fs.readFileSync(path);
+    if (!robotstxt) {
+        throw new Error('No path provided for robots.txt file');
+    }
+
+    if (options.cache) {
+        var maxAge = options.cacheMaxAge;
+        var headers = {
+            'Content-Type': 'text/plain',
+            'Content-Length': robotstxt.length,
+            'ETag': '"' + crypto.createHash('md5').update(robotstxt).digest('hex') + '"',
+            'Cache-Control': 'public, max-age=' + (maxAge / 1000)
+        };
+    }
+
+    return function middleware(req, res, next) {
+        if ('/robots.txt' === req.url) {
+            if (options.cache) {
+                res.writeHead(200, headers);
+            }
+            res.end(robotstxt);
+        } else {
+            return next();
+        }
+    };
+}
+
+module.exports.middleware = middleware;
+
+module.exports = robotstxt;
